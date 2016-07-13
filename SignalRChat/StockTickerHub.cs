c4ae02acb7a76d35fec4b0cb1099c18e028241bb
chingyzhang <chingyzhang@163.com>
2016-07-13 17:43:24 +0800

    SignalR 2 Demo:服务器广播消息

---
+++ SignalRChat/StockTickerHub.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace SignalRChat
+{
+    [HubName("stockTickerMini")]//设置JS代码中调用的名字，默认为类名的驼峰法名称
+    public class StockTickerHub : Hub
+    {
+        private readonly StockTicker _stockTicker;
+
+        public StockTickerHub() : this(StockTicker.Instance) { }
+
+        public StockTickerHub(StockTicker stockTicker)
+        {
+            _stockTicker = stockTicker;
+        }
+
+        public IEnumerable<Stock> GetAllStocks()
+        {
+            return _stockTicker.GetAllStocks();
+        }
+    }
+}
